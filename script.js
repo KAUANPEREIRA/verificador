53fdb7f761cf62a762dd7b6ca4dd7337d1e36a78
KAUANPEREIRA <kauanauan71@gmail.com>
2020-09-30 18:26:59 -0300

    site feito utilizando um pouco de javascript

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-let idade=22
-if(idade<=18){
-    console.log('voce ainda ñ pode tirar habilita')
-}else{
-    console.log('voce pode tirar habilita')
-}
+function verificar(){
+    var ano=new Date()
+    var anoAtual=ano.getFullYear()
+
+    var idade=document.querySelector('#idade')
+    
+    var imagem= document.querySelector('.img')
+    var res=document.querySelector('.res')
+    var i =Number(idade.value)
+    
+    //parte logica
+    
+    if(i==0 || i>anoAtual){
+        alert('verifique os dados e tente novamente')
+    }else{ 
+       var fsex=document.getElementsByName('radsex')
+       var idadeFinal= anoAtual- i
+       var genero=' '
+       if(fsex[0].checked){
+           genero='homen'
+            if(idadeFinal>1 && idadeFinal<10){
+                
+                res.innerHTML=` criança com a idade de ${idadeFinal} anos`
+            }
+            if(idadeFinal<21){
+                imagem.src='jovemH.png'
+                res.innerHTML=` jovem com a idade de ${idadeFinal} anos`
+            }
+            if(idadeFinal<65){
+                imagem.src='homen.png'
+                res.innerHTML=` ${genero} com a idade de ${idadeFinal} anos`
+            }
+            if(idadeFinal>=65){
+                imagem.src='idoso.png'
+                res.innerHTML=` idoso com a idade de ${idadeFinal} anos`
+            }
+       }else if(fsex[1].checked){
+           genero='mulher'
+            if(idadeFinal>1 && idadeFinal<10){
+                imagem.src='bebeM.png'
+                res.innerHTML=` criança com a idade de ${idadeFinal} anos`
+            }
+            if(idadeFinal<21){
+                imagem.src='jovemM.png'
+                res.innerHTML=` jovem com a idade de ${idadeFinal} anos`
+            }
+            if(idadeFinal<65){
+                imagem.src='mulher.png'
+                res.innerHTML=` ${genero} com a idade de ${idadeFinal} anos`
+            }
+            if(idadeFinal>=65){
+                imagem.src='idosa.png'
+                res.innerHTML=` idoso com a idade de ${idadeFinal} anos`
+            }
+       }
+
+       
+}}
+
+
+  
